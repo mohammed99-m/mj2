09bab7e3235595a7bf5cd9ef52fcee9dc576a931
mohammed99-m <markoriobrazil@gmail.com>
2025-09-24 19:13:24 +0300

    edit

--- edub/settings.py
+++ edub/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,6 +59,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
 
+CORS_ALLOW_CREDENTIALS = True
 ROOT_URLCONF = 'edub.urls'
 
 TEMPLATES = [
